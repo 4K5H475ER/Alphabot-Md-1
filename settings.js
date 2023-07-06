f683880bcd3ae8c5581b1a33a13a4c92afcfb1a1
AkShAy GaNz <96863573+4K5H475ER@users.noreply.github.com>
2023-07-06 22:47:28 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -55,17 +55,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 // Other
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Makassar' //  timezone wib
+global.ownername = "ᴹᴿ᭄ ī.aꪶ»✯𝑨𝒌𝒔𝒉𝒂𝒀✯ꫂ ×፝֟͜×" //nama kalian
+global.myweb = "https://s.id/AkshayGanzofc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@AkshayGanzOfc" //bebas asal jan hapus
+global.myweb2 = "https://s.id/AkshayGanzofc" //bebas
+global.email = "akshayganz@gmail.com" //bebas
+global.region = "India" //bebas
+global.timezone = 'Asia/Kolkata' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
 global.badword_list = ["asu", "anjir"] //tambahin sendiri kata² nya, gunakan huruf kecil
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62887435047326"] //ganti agar fitur owner bisa di gunakan
+global.premium = ['919995153271'] //premium user
+global.owner = ["919995153271", "919995153271"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = owner[0] + "@s.whatsapp.net"
 global.ownernomerr = "+"+ownernomer //tidak perlu di edit
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
